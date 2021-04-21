397ec660be052247c381a09f038e4f8dea2ddbe0
Harshit Sharma <harshitsharmabtp@gmail.com>
2021-04-21 14:40:35 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// my routes
+//const userRoutes = require('./routes/Users/user');
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB IS CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+mongoose.Promise = global.Promise;
+
+//My Routes
+//app.use("/user",userRoutes);
+
+app.use("/",(req,res)=>{                 //default route
+  res.send("Welcome to User Authentication System");
+})
+
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`App is running at port ${port}`);
+});
